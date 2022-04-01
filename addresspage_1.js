6a9e0c0c046998f8bfc6acab5ff89f59a1010ca0
Rohan Tekade <rohantekade@gmail.com>
2022-04-01 12:29:16 +0530

    Address Page

---
+++ addresspage_1.js
@@ -0,0 +1,28 @@
+
+let arraydata=[];
+let btn=document.getElementById("submit")
+
+btn.addEventListener("click",function (event){
+    event.preventDefault();
+    var name = document.getElementById("name1").innerHTML;
+
+    var address = document.getElementById("address1").value;
+    console.log(arraydata);
+
+    var mobile = document.getElementById("mobile1").value;
+    console.log(arraydata);
+
+
+    var data = {
+        name:name,
+        address:address,
+        mobile:mobile
+    };
+
+    arraydata.push(data);
+    console.log(arraydata);
+    localStorage.setItem("datavalue",JSON.stringify(arraydata));
+
+    window.location.href = "/Payment.html";
+});
+
