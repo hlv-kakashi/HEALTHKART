145554da99165606ba57b1ba7bda27f0dc17fed3
Surya737673 <99668540+Surya737673@users.noreply.github.com>
2022-04-01 11:09:24 +0530

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -22,24 +22,38 @@ return `<div class="container">
                     <p>Loyalty Rewards</p>
                 </div>
                 <p class="border1"></p>
-                <ul>
-                    <!-- <li> -->
+                
+                    
                         <div class="item-2"><p>Customer Support</p></div>
-                        <div class="hide">
-                        <ul style="width:300px;height:300px">
-                        <li style="font-size: 14px; font-weight: bold;"><a   href="">Contact Us</a></li>
-                        <li style="font-size: 14px; font-weight: bold;"><a   href="">Helpful Links</a></li>
-                        <li style="font-size: 14px;"><a   href="">Chat With US</a></li>
-                        <li style="font-size: 14px;"><a   href="">Track Your Order</a></li>
-                        <li style="font-size: 14px;"><a   href="">Email Us</a></li>
-                        <li style="font-size: 14px;"><a   href="">Return Policy</a></li>
-                        <li style="font-size: 14px;"><a   href="">Ask Our Experts</a></li>
-                        <li style="font-size: 14px;"><a   href="">FAQ</a>
-                        
-                        </ul>
+                        <div id="hide">
+                        <div id="all">
+    <div id="m">
+
+        <div id="one">
+            <h4>Contact us</h4>
+
+            <a href="">Chat</a> <br>
+            <a href="">Email</a><br>
+            <a href="">Ask our support</a>
+        </div>
+        <hr style="height: 100px; margin-top: 50px; margin-right: 20px;">
+        <div id="two">
+            <h4>Helpful links</h4><br>
+            <a href="">Track your orders</a><br>
+            <a href="">Return Policy</a><br>
+            <a href="">Faq</a>
+        </div>
+    </div>
+        <div id="three">
+            <h5 >For Support & Order Enquiries<br>
+            Call us at : 0124-4616444<br>
+            10 AM to 7 PM</h5>
+        </div>
+    </div>
+    
                       </div>
-                    <!-- </li> -->
-                </ul>
+                    
+                
                 <div class="arrow2">
                     <img src="https://static1.hkrtcdn.com/hknext/static/media/common/header/arrow-down.svg" alt="">
                 </div>
@@ -51,60 +65,39 @@ return `<div class="container">
                       src="https://static1.hkrtcdn.com/hknext/static/media/common/header/acc-icon.svg"
                     />
                 </div>
-                <ul>
-                    <!-- <li> -->
+                
+                    <!--  -->
                       <div class="item-3" id="go_to_login_page">
                         <a href="login.html"><p>My Accounts and Orders</p></a>
                       </div>
-                      <div class="hide">
-                        <ul style="width:300px;height:300px";>
-                        <li style="width:300px;height:50px">
-                          <button class="loginbutton" onclick="myLoginFunction()">Login</button>
-                        </li>
-                        <br>
-                        <li style="width:300px;height:50px;margin-top: 10px;">
-                          <button class="newuserbutton" onclick="myLoginFunction()">New User? / Sign Up</button>
-                        </li>
-                        <br>
-                        <li>
-                          <p style="color:  #00c2c1;">My Account</p>
-            
-                        </li>
-                        <li>
-                          <p style="color:  #00c2c1;">Health Profile</p>
-                          
-                        </li>
-                        <li>
-                          <p>Orders</p>
-                          
-                        </li>
-                        <li>
-                          <p>Diet Plan</p>
-                          
-                        </li>
-                        <li>
-                          <p>Wishlist</p>
-                          
-                        </li>
-                        <li>
-                          <p>My Consultations</p>
-                          
-                        </li>
-                        <li>
-                          <p>Royalty Reward</p>
-                          
-                        </li>
-                        <li>
-                          <p>Recommended Product</p>
-                          
-                        </li>
-                        <br>
-                        <br>
-                        
-                        
-                      </ul>
-                      <!-- </li> -->
-                      </ul>
+                      <div id="hide1">
+                      <div id="account">
+        <button style="border-radius: 5px; height: 30px; width: 100%; background-color: teal;">Login</button><br>
+        <button style="border-radius: 5px; height: 30px; width: 100%; margin-top: 5px;">Sign up</button>
+
+        <div> <div id="all">
+            <div id="m">
+        
+                <div id="one">
+                    <h4>My Account</h4>
+        
+                    <a href="">Orders</a> <br>
+                    <a href="">Wishlist</a><br>
+                    <a href="">Loyalty Rewards</a>
+                </div>
+                <hr style="height: 100px; margin-top: 50px; margin-right: 20px;">
+                <div id="two">
+                    <h4>Health Profile</h4><br>
+                    <a href="">Diet Plans</a><br>
+                    <a href="">My Consultations</a><br>
+                    <a href="">Recommended Products</a>
+                </div>
+            </div>
+                
+            </div>   
+    </div>
+   
+                      </div>
                     </div>
                     <div class="arrow">
                       <img src="https://static1.hkrtcdn.com/hknext/static/media/common/header/arrow-down.svg" alt="">
@@ -126,108 +119,107 @@ return `<div class="container">
 
     <div id="nav" class="navbar2">
       
-        <!-- <li>  -->
-          <div class="nav-items-1"><p>Category</p>
-            <div class="hide">
-          <ul>
-            <li><a   href="">Sports Nutrition</a></li>
-            <li><a   href="">Vitamins & Supplements</a></li>
-            <li><a   href="">Ayurveda & Herbs</a></li>
-            <li><a   href="">Health Food & Drinks</a></li>
-            <li><a   href="">Fitness</a></li>
-            <li><a   href="">Wellness</a></li>
-            <li><a   href="">Service Program</a></li>
+        <!--   -->
+          <div class="nav-items-1">Category</div>
+            <div id="hide2">
+          
+            <a   href="">Sports Nutrition</a><br>
+            <a   href="">Vitamins & Supplements</a><br>
+            <a   href="">Ayurveda & Herbs</a><br>
+            <a   href="">Health Food & Drinks</a><br>
+            <a   href="">Fitness</a><br>
+            <a   href="">Wellness</a><br>
+            <a   href="">Service Program</a>
             
-          </ul>
+          
+            
         </div>
-        </div>
-        <!-- </li> -->
-          <!-- <li>  -->
+       
             <div class="nav-items-2"><p>Brand</p> 
               <div class="hide">
 
-            <ul>
-              <li><a  href="">MuscleBlaze <br>     </a></li>
-              <li><a  href="">TrueBasics</a></li>
-              <li><a  href="">JustHer</a></li>
-              <li><a  href="">Nouriza</a></li>
-              <li><a  href="">Gritzo</a></li>
-              <li><a  href="">bGREEN</a></li>
-              <li><a  href="">GNC</a></li>
-              <li><a  href="">NOW</a></li>
-              <li><a  href="">Healthvit</a></li>
-              <li><a  href="">Yoga Bar</a></li>
-              <li><a  href="">Amway</a></li>
-              <li><a  href="">Natures Velvet</a></li>
+            
+              <a  href="">MuscleBlaze <br>     </a>
+              <a  href="">TrueBasics</a>
+              <a  href="">JustHer</a>
+              <a  href="">Nouriza</a>
+              <a  href="">Gritzo</a>
+              <a  href="">bGREEN</a>
+              <a  href="">GNC</a>
+              <a  href="">NOW</a>
+              <a  href="">Healthvit</a>
+              <a  href="">Yoga Bar</a>
+              <a  href="">Amway</a>
+              <a  href="">Natures Velvet</a>
               
-            </ul>
+            
           </div>
           </div>
-          <!-- </li> -->
-          <!-- <li> -->
+          <!--  -->
+          <!--  -->
             <div class="nav-items-3"><p>Gender</p>
               <div class="hide">
             <ul style="width:200px;height:100px";>
-              <li><a   href=""> <img src="https://static1.hkrtcdn.com/hknext/static/media/common/header/man-2.svg"></a></li>
-              <li><a   href=""><img src="https://static1.hkrtcdn.com/hknext/static/media/common/header/woman-2.svg"></a></li>
+              <a   href=""> <img src="https://static1.hkrtcdn.com/hknext/static/media/common/header/man-2.svg"></a>
+              <a   href=""><img src="https://static1.hkrtcdn.com/hknext/static/media/common/header/woman-2.svg"></a>
       
-            </ul>
+            
           </div>
           </div>
-          <!-- </li> -->
-          <!-- <li> -->
+          <!--  -->
+          <!--  -->
             <div class="nav-items-4"><p>Goal</p>
               <div class="hide">
             <ul style="width:700px;height:300px; ";>
-              <li ><a   href=""><img style="width:80px;height:80px"; src="https://img6.hkrtcdn.com/14784/normal_1478355_o.png"></a></li>
-              <li><a   href=""><img style="width:80px;height:80px"; src="https://img8.hkrtcdn.com/14784/normal_1478357_o.png"></a></li>
-              <li><a   href=""><img style="width:80px;height:80px"; src="https://img10.hkrtcdn.com/14784/normal_1478359_o.png"></a></li>
-              <li><a   href=""><img style="width:80px;height:80px"; src="https://img2.hkrtcdn.com/14784/normal_1478361_o.png"></a></li>
-              <li><a   href=""><img style="width:80px;height:80px"; src="https://img4.hkrtcdn.com/14784/normal_1478363_o.png"></a></li>
+              <li ><a   href=""><img style="width:80px;height:80px"; src="https://img6.hkrtcdn.com/14784/normal_1478355_o.png"></a>
+              <a   href=""><img style="width:80px;height:80px"; src="https://img8.hkrtcdn.com/14784/normal_1478357_o.png"></a>
+              <a   href=""><img style="width:80px;height:80px"; src="https://img10.hkrtcdn.com/14784/normal_1478359_o.png"></a>
+              <a   href=""><img style="width:80px;height:80px"; src="https://img2.hkrtcdn.com/14784/normal_1478361_o.png"></a>
+              <a   href=""><img style="width:80px;height:80px"; src="https://img4.hkrtcdn.com/14784/normal_1478363_o.png"></a>
               
-            </ul>
+            
           </div>
           </div>
-          <!-- </li> -->
-          <!-- <li> -->
+          <!--  -->
+          <!--  -->
             <div class="nav-items-5"><p>Bestsellers</p>
               <div class="hide">
-            <ul>
-              <li><a   href="">Sport Nutrition</a></li>
-              <li><a   href="">Protein Poweder</a></li>
-              <li><a   href="">Whey Proteins</a></li>
-              <li><a   href="">Mass Gainer</a></li>
-              <li><a   href="">Fat Burners</a></li>
-              <li><a   href="">Pre WorkOut</a></li>
-              <li><a   href="">Protein Bars</a></li>
-              <li><a   href="">Weight Gainer</a></li>
-              <li><a   href="">Carb Blends</a></li>
-              <li><a   href="">Other Supports</a></li>
-            </ul>
+            
+              <a   href="">Sport Nutrition</a>
+              <a   href="">Protein Poweder</a>
+              <a   href="">Whey Proteins</a>
+              <a   href="">Mass Gainer</a>
+              <a   href="">Fat Burners</a>
+              <a   href="">Pre WorkOut</a>
+              <a   href="">Protein Bars</a>
+              <a   href="">Weight Gainer</a>
+              <a   href="">Carb Blends</a>
+              <a   href="">Other Supports</a>
+            
           </div>
         </div>
-          <!-- </li> -->
-          <!-- <li> -->
+          <!--  -->
+          <!--  -->
             <div class="nav-items-6"><p>Deals</p>
               <div class="hide">
             <ul style="width:700px;height:300px ";>
-              <li><a   href=""><img style="width:120px;height:60px"; src="https://img4.hkrtcdn.com/14768/normal_1476713_o.png"></a></li>
-              <li><a   href=""><img style="width:120px;height:60px"; src="https://img8.hkrtcdn.com/14768/normal_1476717_o.png"></a></li>
-              <li><a   href=""><img style="width:120px;height:60px"; src="https://img2.hkrtcdn.com/14768/normal_1476721_o.png"></a></li>
-              <li><a   href=""><img style="width:120px;height:60px"; src="https://img10.hkrtcdn.com/14768/normal_1476719_o.png"></a></li>
-              <li><a   href=""><img style="width:120px;height:60px"; src="https://static1.hkrtcdn.com/hknext/static/media/common/misc/crazy.svg"></a></li>
-            </ul>
+              <a   href=""><img style="width:120px;height:60px"; src="https://img4.hkrtcdn.com/14768/normal_1476713_o.png"></a>
+              <a   href=""><img style="width:120px;height:60px"; src="https://img8.hkrtcdn.com/14768/normal_1476717_o.png"></a>
+              <a   href=""><img style="width:120px;height:60px"; src="https://img2.hkrtcdn.com/14768/normal_1476721_o.png"></a>
+              <a   href=""><img style="width:120px;height:60px"; src="https://img10.hkrtcdn.com/14768/normal_1476719_o.png"></a>
+              <a   href=""><img style="width:120px;height:60px"; src="https://static1.hkrtcdn.com/hknext/static/media/common/misc/crazy.svg"></a>
+            
           </div>
         </div>
-          <!-- </li> -->
-          <!-- <li> -->
+          <!--  -->
+          <!--  -->
             <div class="nav-items-7"><p><a href="giftcard.html">Gift Card</a></p></div>
   
-          <!-- </li> -->
-          <!-- <li> -->
+          <!--  -->
+          <!--  -->
             <div class="nav-items-8"><p><a href="blog.html">Blog, Videos & More</a></p></div>
-          <!-- </li> -->
-        </ul>
+          <!--  -->
+        
      
   
       <div class="rightPart">
